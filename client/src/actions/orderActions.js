33eb2a36c7bdfedc36abe1cc52432692a3bdc218
belalelmi <belalelmi@gmail.com>
2022-01-25 18:43:09 -0500

    fixed issue where another user can see others orders

--- client/src/actions/orderActions.js
+++ client/src/actions/orderActions.js
@@ -5,7 +5,13 @@ import {
   ORDER_CREATE_FAIL,
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
-  ORDER_DETAILS_FAIL
+  ORDER_DETAILS_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
+  ORDER_PAY_FAIL,
+  ORDER_MY_LIST_REQUEST,
+  ORDER_MY_LIST_SUCCESS,
+  ORDER_MY_LIST_FAIL
 } from '../constants/orderConstants'
 import { logout } from './userActions'
 
@@ -77,6 +83,80 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
     }
     dispatch({
       type: ORDER_DETAILS_FAIL,
+      payload: message,
+    })
+  }
+}
+
+export const payOrder = (orderId, paymentResult) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_PAY_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(`/api/orders/${orderId}/pay`, paymentResult, config)
+
+    dispatch({
+      type: ORDER_PAY_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    if (message === 'Not authorized, token failed') {
+      dispatch(logout())
+    }
+    dispatch({
+      type: ORDER_PAY_FAIL,
+      payload: message,
+    })
+  }
+}
+export const ListMyOrders = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_MY_LIST_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.get(`/api/orders/myorders`, config)
+
+    dispatch({
+      type: ORDER_MY_LIST_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    if (message === 'Not authorized, token failed') {
+      dispatch(logout())
+    }
+    dispatch({
+      type: ORDER_MY_LIST_FAIL,
       payload: message,
     })
   }
--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -3,6 +3,7 @@ import {
   USER_DETAILS_FAIL,
   USER_DETAILS_REQUEST,
   USER_DETAILS_SUCCESS,
+  USER_DETAILS_RESET,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
@@ -13,8 +14,8 @@ import {
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS
-
 } from "../constants/userConstants"
+import { ORDER_MY_LIST_RESET } from "../constants/orderConstants"
 
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -56,6 +57,8 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo')
   dispatch({ type: USER_LOGOUT })
+  dispatch({ type: USER_DETAILS_RESET })
+  dispatch({ type: ORDER_MY_LIST_RESET })
 }
 
 export const register = (name, email, password) => async (dispatch) => {
@@ -95,7 +98,8 @@ export const register = (name, email, password) => async (dispatch) => {
           ? error.response.data.message
           : error.message,
     })
-  }}
+  }
+}
 
 
 export const getUserDetails = (id) => async (dispatch, getState) => {
@@ -167,4 +171,5 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
           ? error.response.data.message
           : error.message,
     })
-  }}
+  }
+}
