d3361b35adc9a0b7fd14bc6a0634297fe004893d
belalelmi <belalelmi@gmail.com>
2022-01-25 15:12:21 -0500

    added route for updating order payment status

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 
 const router = express.Router();
-import { addOrderItems, getOrderById } from "../controllers/orderController.js";
+import { addOrderItems, getOrderById, updateOrderToPaid } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, addOrderItems)
 router.route('/:id').get(getOrderById)
+router.route('/:id/pay').put(updateOrderToPaid)
+
 
 export default router;
